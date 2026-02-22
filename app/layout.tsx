0ed4b1ed4d41e223873caf66ce7c5a82852dcf54
Kishore Gurusamy <kishore08052003@gmail.com>
2026-02-22 15:22:08 +0530

    removed navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { Toaster } from "react-hot-toast";
-import Navbar from "./components/navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
